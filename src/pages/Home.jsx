2c89be289419eb0b639c3e2ad4bd989bdeaeb086
SIRAJUDEEN-I <sirajudeen4300@gmail.com>
2025-06-15 10:04:28 +0530

    added stars and meteor background

---
+++ src/pages/Home.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import  ThemeToggle  from '../components/ThemeToggle'
+import StarBackground from '../components/StarBackground'
+const Home = () => {
+  return (
+    <div className='min-h-screen'>
+{/* {theme} */}
+<ThemeToggle/>
+{/* {background effects} */}
+
+<StarBackground />
+
+{/* Navbar */}
+
+{/* Main content */}
+
+{/* Footer */}
+
+    </div>
+  )
+}
+
+export default Home
